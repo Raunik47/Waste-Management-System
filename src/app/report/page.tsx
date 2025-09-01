792bd514d8002e97474c89f8a45d4a733f7a8877
Raunik47 <rauniksingh787878@gmail.com>
2025-09-02 02:16:42 +0530

    adding the input bos=x for type of waste

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -356,7 +356,7 @@ export default function ReportPage() {
           </div>
           <div>
             <label htmlFor="type" className="block text-sm font-medium text-gray-700 mb-1">Waste Type</label>
-            {/* <input
+            <input
               type="text"
               id="type"
               name="type"
@@ -366,7 +366,7 @@ export default function ReportPage() {
               className="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-green-500 transition-all duration-300 bg-gray-100"
               placeholder="Verified waste type"
               readOnly
-            /> */}
+            />
           </div>
           <div>
             <label htmlFor="amount" className="block text-sm font-medium text-gray-700 mb-1">Estimated Amount</label>
